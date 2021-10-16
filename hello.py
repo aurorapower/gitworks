a457218ae218c862611d4c83b7b1e377f59093ef
Andy <gongtiejing@gmail.com>
2021-10-15 21:53:17 -0300

    add a new test
    Co-Authored-By: Andrew Zaitsev-Zotov <3235+Andy@users.noreply.github.com>

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+name = input('please enter your name:')
+print('your name is :', name)
+# print absolute value of an integer:
+a = 100
+
+if a >=0:
+    print(a)
+else:
+    print(-a)
